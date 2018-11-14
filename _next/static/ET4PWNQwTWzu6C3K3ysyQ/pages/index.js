093b03d8983d67e6ecd955b750d315201f33313e
Robert Ferguson <rob@rewferguson.com>
2018-11-13 23:36:32 -0600

    add home link

---
+++ _next/static/ET4PWNQwTWzu6C3K3ysyQ/pages/index.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[12],{275:function(t,e,n){__NEXT_REGISTER_PAGE("/",function(){return t.exports=n(276),{page:t.exports.default}})},276:function(t,e,n){"use strict";n.r(e);var o=n(0),r=n.n(o),u=n(24);e.default=Object(u.withRouter)(function(t){t.router,t.props;return r.a.createElement("section",null)})}},[[275,1,0]]]);
